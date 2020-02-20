93de17bd4b16c0033988674a508174595768448e
Nic Gerhardt <NicolasGerhardt@quickenloans.com>
2020-02-20 14:09:35 -0500

    Make Change and Till are all working as expected.

--- GC_Lab_6/CashRegister.cs
+++ GC_Lab_6/CashRegister.cs
@@ -6,5 +6,29 @@ namespace GC_Lab_6
 {
     public class CashRegister
     {
+        public double Till { get; private set; }
+
+        public CashRegister(double startingTill)
+        {
+            Till = startingTill;
+        }
+
+
+        /// <summary>
+        /// Will lower the amount in the t
+        /// </summary>
+        /// <param name="requestAmount"></param>
+        /// <returns></returns>
+        public bool TryMakeChange(double requestAmount)
+        {
+            if (requestAmount > Till)
+            {
+                return false;
+            }
+
+            Till -= requestAmount;
+            
+            return true;
+        }
     }
 }
--- GC_Lab_6_Test/CashRegister_Test.cs
+++ GC_Lab_6_Test/CashRegister_Test.cs
@@ -2,12 +2,61 @@
 using System.Collections.Generic;
 using System.Text;
 using GC_Lab_6;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Xunit;
 
 namespace GC_Lab_6_Test
 {
-    class CashRegister_Test
+    public class CashRegister_Test
     {
+        [Fact]
+        public void Till_HasCash_NotEmpty()
+        {
+            var register = new CashRegister(500.00);
 
+            double expected = 500.0;
+
+            double actual = register.Till;
+
+            Assert.Equal(expected, actual, 2);
+        }
+
+        [Fact]
+        public void MakeChange_RequestAmountSmallerThanTill_ReturnFullAmount()
+        {
+            var register = new CashRegister(500.00);
+            double requestAmount = 10.0;
+
+            bool actual = register.TryMakeChange(requestAmount);
+
+
+            Assert.True(actual);
+        }
+
+        [Fact]
+        public void MakeChange_RequestAmountSmallerThanTill_TillAmountLowered()
+        {
+            var register = new CashRegister(500.00);
+            double requestAmount = 10.0;
+            double expected = 500 - requestAmount;
+
+            register.TryMakeChange(requestAmount);
+            double actual = register.Till;
+
+            Assert.Equal(expected, actual, 2);
+        }
+
+        [Fact]
+        public void MakeChange_RequestAmountSmallerThanTill_negitive1()
+        {
+            var register = new CashRegister(5.00);
+            double requestAmount = 10.0;
+
+            bool actual = register.TryMakeChange(requestAmount);
+
+
+            Assert.False(actual);
+        }
+        
     }
 }
